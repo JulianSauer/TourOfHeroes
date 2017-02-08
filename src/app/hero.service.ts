d922318a5b35ade635d516732ff7cbaa394db6e9
JulianSauer <julian_sauer@gmx.net>
2017-02-08 15:39:15 +0100

    Moved hero data to a seperate serivce.

---
+++ src/app/hero.service.ts
@@ -0,0 +1,12 @@
+import {Injectable} from "@angular/core";
+import {HEROES} from "./mock-heroes";
+import {Hero} from "./hero";
+
+@Injectable()
+export class HeroService {
+
+  getHeroes(): Promise<Hero[]> {
+    return Promise.resolve(HEROES);
+  }
+
+}
