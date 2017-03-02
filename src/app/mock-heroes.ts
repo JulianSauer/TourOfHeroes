763fda393b1fc2bf435f6cc591cfa0c873cc4803
JulianSauer <julian_sauer@gmx.net>
2017-03-02 16:22:40 +0100

    Added a new hero to test the deployment process.

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,6 +1,7 @@
 import {Hero} from "./hero";
 
 export const HEROES: Hero[] = [
+  {id: 10, name: 'New Hero'},
   {id: 11, name: 'Mr. Nice'},
   {id: 12, name: 'Narco'},
   {id: 13, name: 'Bombasto'},
